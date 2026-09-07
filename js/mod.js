14310dd4f1189258cb6117b9e27b1b410d5a66e5
Moos <mees.engelen@gmail.com>
2026-09-07 16:18:35 +0200

    changelog

--- js/mod.js
+++ js/mod.js
@@ -21,7 +21,11 @@ let VERSION = {
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.1 | "September 5th 2026" - Release</h3><br>
 		- 3 Layers.<br>
-		- A lot of BEANS.`
+		- A lot of BEANS <br><br>
+	<h3>v0.11 | "September 7th 2026" - Mandatory bug fixes + QoL Update </h3><br>
+		- corrected lab ratios; multiple combinations are possible now <br>
+		- added presets once the recipe is found <br>
+		- and other smaller changes`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
